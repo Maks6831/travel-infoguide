74a2c2a0347de1cc962d5dc82bb5faf94a8c759c
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-02-07 21:28:21 +0000

    added possible pseudocode

--- script.js
+++ script.js
@@ -16,10 +16,36 @@ $.ajax(settings).then(function (response) {
     console.log(response);
 });
 
+/*
+function destinationInfo(searchinput){
+    ajax call to travel advisor
+    append on to page
+}
+
+function destinationHotels(searchinput){
+    ajax call to hotel api (maybe we can use travel advisor for this)
+    append on to page
+}
+
+function destinatNews(searchinput){
+    ajax call to news api 
+    append on to page. 
+}
 
 
 
 
 
 
+ search button event listener function(){
+
+    destinationInfo(searchinput) -----> function for acquiring destination info
+    destinationHotels(searchinput) ---------> function for acquiring hotels
+    destinatNews(searchinput) --------------> function for acquiring news
+
+}
+
+
+also i think we need to create a function which stores the search history. 
+*/
 })
