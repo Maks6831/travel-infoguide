45eb4436187ec531f28f23d3ff15d737326620d0
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-02-07 21:10:50 +0000

    travel advisor url is now working

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 
-
 $(function(){
-    console.log("hello world!");
+console.log("Travel Advisor Api... ");
+const settings = {
+     "async": true,
+     "crossDomain": true,
+     "url": "https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=eiffel%20tower&lang=en_US&units=km",
+     "method": "GET",
+     "headers": {
+         "X-RapidAPI-Key": "d5d189dccbmsh2ab4d6cb457627ap192a53jsn3fd7cd6caa99",
+         "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+     }
+};
+    
+$.ajax(settings).then(function (response) {
+    console.log(response);
+});
+
+
+
+
+
+
 
 })
