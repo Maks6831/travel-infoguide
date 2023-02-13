0af56dca1b96c1a010a3a6cc03275577cf2640d6
Mat Crawshaw <mat_crawshaw@hotmail.com>
2023-02-13 14:25:50 +0000

    added places page and linking buttons

---
+++ saved_places.js
@@ -0,0 +1,32 @@
+let favouritesArray = JSON.parse(window.localStorage.getItem("travelFavourites")) ?? [];
+console.log(favouritesArray);
+
+
+
+
+function renderFavourites() {
+    $("#favourites").empty();
+
+let faveRow = $("<div>").addClass("row");
+
+for (let i=0; i<favouritesArray.length; i++) {
+ 
+    let faveCard= $("<div>").addClass("cards card faveCards").css("background-image", "url(" + favouritesArray[i][2] + ")")
+  // let faveImage = $("<img>").attr("src", favouritesArray[i][2]).addClass("faveImage");
+   let cardText = $("<div>").addClass("faveCardBackground");
+    let favePlace = $("<h6>").text(favouritesArray[i][1]).addClass("faveCardText card-text");
+    let faveCity = $("<h6>").text(favouritesArray[i][0]).addClass("faveCardText card-text");
+
+
+    cardText.append(favePlace, faveCity);
+    faveCard.append(cardText);
+    faveRow.append(faveCard);
+    $("#favourites").append(faveRow);
+}
+}
+
+if (favouritesArray.length !== 0) {
+renderFavourites()
+} else {
+
+}
